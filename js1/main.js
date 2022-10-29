bf7153219bf816ad0923e45c821e2410fd413c67
Wallcraft <102182020+Wallcraftt@users.noreply.github.com>
2022-10-29 15:13:36 +0400

    added js

---
+++ js1/main.js
@@ -0,0 +1,51 @@
+// Task 1)
+
+const currentDay = new Date('October 30 2022',);
+const day1 = currentDay.getDay();
+
+console.log(day1);
+
+let weekDays = ['sunday','monday','tuesday','wednesday','thursday', 'friday','sunday']
+
+switch (weekDays) {
+  case weekDays:
+    console.log(weekDays)
+
+  default:
+    break;
+}
+
+// Task 2)
+for (let i = 0; i < 50; i++) {
+  console.log(i)
+}
+// Task 3) 
+let x = 0
+while (x < 150) {
+  x+=1 
+  console.log(x)
+  
+}
+
+// Task 4)
+y = 0
+do {
+  console.log(y)
+  y++
+} while (y < 100);
+
+// Task 5)
+
+let anEmptyArray = []
+
+for ( let  h = 1;  h < 100; h++) {
+  anEmptyArray.push(h*h);
+}
+
+console.log(anEmptyArray)
+
+
+
+
+
+
